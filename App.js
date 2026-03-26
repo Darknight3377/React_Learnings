fd281e14a136c82b0c7a44dbb7ce53ae1758264e
debasmita-roy <debo2018new@gmail.com>
2026-03-26 19:35:33 +0530

    add basics react codebase

---
+++ App.js
@@ -0,0 +1,13 @@
+
+const parent = React.createElement("div", {id: "parent"},
+    React.createElement("div", {id: "child"}, 
+    [React.createElement("h1", {}, "I am a h1 tag"),React.createElement("h1", {}, "I am a h2 tag") ]
+    )
+)
+const heading = React.createElement("h1", {"id": "heading"}, "Hello world from react!");
+
+console.log(heading); // It is just an object 
+
+        const root = ReactDOM.createRoot(document.getElementById("root"));
+        root.render(parent); // puts the object and create h1 tag to render in ui
+        //  Replace anything present in id root not appended
